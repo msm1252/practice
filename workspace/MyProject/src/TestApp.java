aa180bf984bd2c3d40f0ae1f033b76f3e006da47
Subbu <msmb4u@gmail.com>
2018-07-20 09:19:21 +0530

    Initicommit

---
+++ workspace/MyProject/src/TestApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author msmb4
+ *
+ */
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi I am in main class");
+	}
+
+}
